129814a24d0453c1e4418d8d6c805077e1469aaa
Mateo Gomez <mateo.gomez.santa@gmail.com>
2022-02-08 17:43:00 -0500

    Header update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('header-nav');
 const closeButton = document.getElementById('close');
+const header = document.getElementById('header-wrapper');
 
 console.log(hamburger, navUL);
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
-})
+});
+
+let prevScroll = window.pageYOffset;
+window.addEventListener('scroll', () => {
+    let currentScroll = window.pageYOffset;
+
+    if(prevScroll < currentScroll){
+        header.classList.add('hide');
+    }else{
+        header.classList.remove('hide');
+    }
+
+    prevScroll = currentScroll;
+});
